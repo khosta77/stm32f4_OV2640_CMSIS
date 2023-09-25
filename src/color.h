795e65cf6c856485c17c3bcad5e54a5055da6da5
khosta77 <puwerfulpants@gmail.com>
2023-09-25 21:58:02 +0300

    Инициализацию светодиодов вынес в отдельный файл, чтобы перед глазами не маячила

---
+++ src/color.h
@@ -0,0 +1,35 @@
+#ifndef COLOR_H_
+#define COLOR_H_
+
+#include "../system/include/cmsis/stm32f4xx.h"
+
+#define PWM_MODE 0
+
+#if PWM_MODE
+#define G_ON() { GPIOD->ODR |= GPIO_ODR_OD12; }
+#define G_OFF() { GPIOD->ODR &= ~GPIO_ODR_OD12; }
+#endif
+
+#define O_ON() { GPIOD->ODR |= GPIO_ODR_OD13; }
+#define O_OFF() { GPIOD->ODR &= ~GPIO_ODR_OD13; }
+
+#define R_ON() { GPIOD->ODR |= GPIO_ODR_OD14; }
+#define R_OFF() { GPIOD->ODR &= ~GPIO_ODR_OD14; }
+
+#define B_ON() { GPIOD->ODR |= GPIO_ODR_OD15; }
+#define B_OFF() { GPIOD->ODR &= ~GPIO_ODR_OD15; }
+
+void my_GPIO_init();  // Для вывода цветов
+
+void my_GPIO_init() {
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+#if PWM_MODE
+    GPIOD->MODER |= (GPIO_MODER_MODER12_0 | GPIO_MODER_MODER13_0 | GPIO_MODER_MODER14_0 | GPIO_MODER_MODER15_0);
+    GPIOD->ODR &= ~(GPIO_ODR_OD12 | GPIO_ODR_OD13 | GPIO_ODR_OD14 | GPIO_ODR_OD15);
+#else
+    GPIOD->MODER |= (GPIO_MODER_MODER13_0 | GPIO_MODER_MODER14_0 | GPIO_MODER_MODER15_0);
+    GPIOD->ODR &= ~(GPIO_ODR_OD13 | GPIO_ODR_OD14 | GPIO_ODR_OD15);
+#endif
+}
+
+#endif  // COLOR_H_
