99a7dd6d7ea79914db984a24c96e477d4e0c5f2f
khosta77 <puwerfulpants@gmail.com>
2023-09-26 14:27:45 +0300

    Заготовка под OV7725

---
+++ src/OV7725.h
@@ -0,0 +1,7 @@
+#ifndef OV7725_H_ 
+#define OV7725_H_
+
+#include "OVxxxx_foo.h"
+#include "OV7725_register.h"
+
+#endif  // OV7725_H_
---
+++ src/OV7725_register.h
@@ -0,0 +1,4 @@
+#ifndef OV7725_REGISTER_H_ 
+#define OV7725_REGISTER_H_
+
+#endif  // OV7725_REGISTER_H_
